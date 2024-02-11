589768c0ca0a57b8ded56d945e1147fea7506a6c
Nicholas Lenz <nicholaslenz12@g.ucla.edu>
2024-02-10 18:00:42 -0800

    Change priorities

--- toggle_fx.lua
+++ toggle_fx.lua
@@ -2,10 +2,10 @@
 -- global. This way toggle visibility will work irrespective of focus.
 
 -- TODO:
---  Handle case when FX chain is open
---  Handle case when FX chain and track effects are both open (save FX chain first)
 --  Simplify table storing GUID -> FXs
 --  Verify GUID invalidations (don't reload effects from tracks that don't exist)
+--  Handle case when FX chain is open
+--  Handle case when FX chain and track effects are both open (save FX chain first)
 
 local using_reaper = reaper ~= nil
 local current_project = reaper.EnumProjects(-1, "")
